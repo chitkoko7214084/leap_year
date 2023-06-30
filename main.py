2d12c411981ab3300b71a9904f24bfb1608439aa
chit ko <80560570+chitkoko7214084@users.noreply.github.com>
2023-06-29 20:23:36 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+
+#Leap year program
+#Author Chit Ko Ko
+
+
+year = int(input("Which year do you want to check? "))
+
+
+if year%4 == 0:
+    if year%100==0:
+        if year%400==0:
+            print ("Leap year.")
+        else:
+            print ("Not leap year")
+
+
+    
+
+    else:
+        print("leap year.")
+
+
+else:
+    print("Not leap year.")
+    
+
